bf7b2363b9b925754d899e62552a748005a354eb
kulkarnisharvari1709 <kulkarnisharvari1709@gmail.com>
2025-07-28 15:40:22 +0530

    Create Loop_Continue_SkipNumber.cpp

---
+++ Loop_Continue_SkipNumber.cpp
@@ -0,0 +1,13 @@
+//Program 3
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        if (i == 5) {
+            continue; 
+        }
+        cout << i << endl;
+    }
+    return 0;
+}
